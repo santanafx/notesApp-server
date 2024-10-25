610a6a266bbf32fafd31d6b8a033276ef94f7974
lucas <santanafx@hotmail.com>
2024-10-25 08:16:27 -0300

    refactor: added routes from router

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
+import router from "./router";
 
 const app = express();
 
@@ -9,8 +10,6 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/", router);
 
 export default app;

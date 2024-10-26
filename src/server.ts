08906cb9dd77e78337890eb9fdcc26615c7a5410
lucas <santanafx@hotmail.com>
2024-10-26 07:03:20 -0300

    feat: created error handler

--- src/server.ts
+++ src/server.ts
@@ -12,4 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", router);
 
+app.use((err, req, res, next) => {
+  if (err.type === "auth") {
+    res.status(401).json({ message: "unauthorized" });
+  } else if (err.type === "input") {
+    res.status(400).json({ message: "invalid input" });
+  } else {
+    res.status(500).json({ message: "unknown error" });
+  }
+});
+
 export default app;

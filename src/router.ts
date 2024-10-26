18586dfbf297de97174597ab52460b40a17fe039
lucas <santanafx@hotmail.com>
2024-10-26 07:25:02 -0300

    refactor: removed create route from routes

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,7 @@
 import { Router } from "express";
 import { body } from "express-validator";
-import { createNewUser } from "./handlers/user";
 import { handleInputsErrors } from "./modules/middleware";
 
 const router = Router();
-
-router.post(
-  "/signup",
-  body("email").isString(),
-  body("password").isString(),
-  handleInputsErrors,
-  createNewUser
-);
 
 export default router;

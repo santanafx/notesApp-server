b941b57bd13f0756da61c29b8dfd880fe4c527cd
lucas <santanafx@hotmail.com>
2024-11-03 12:23:32 -0300

    feat: created post and get to notes

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,17 @@
 import { Router } from "express";
 import { body } from "express-validator";
+import { createNewNote, getNotes } from "./controllers/note.controller";
 import { handleInputsErrors } from "./modules/middleware";
 
 const router = Router();
+
+router.post(
+  "/createNewNote",
+  body("text").isString(),
+  handleInputsErrors,
+  createNewNote
+);
+
+router.get("/getNotes", getNotes);
 
 export default router;

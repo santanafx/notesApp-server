e2fc14ec048d57f72d2a80bc629878da94018f33
lucas <santanafx@hotmail.com>
2024-11-03 12:24:11 -0300

    feat: new controllers for notes

---
+++ src/controllers/note.controller.ts
@@ -0,0 +1,39 @@
+import prisma from "../db";
+
+export const createNewNote = async (req, res, next) => {
+  try {
+    await prisma.note.create({
+      data: {
+        text: req.body.text,
+        note: {
+          connect: {
+            id: req.user.id,
+          },
+        },
+      },
+    });
+
+    res.json({ message: "note created" });
+  } catch (error) {
+    console.log("createNewNote", error);
+    error.type = "input";
+    next(error);
+  }
+};
+
+export const getNotes = async (req, res, next) => {
+  try {
+    const notes = await prisma.note.findMany({
+      where: {
+        note: {
+          id: req.user.id,
+        },
+      },
+    });
+    res.json({ message: "notes", notes });
+  } catch (error) {
+    console.log("getNotes", error);
+    error.type = "input";
+    next(error);
+  }
+};

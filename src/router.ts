1e33b3e90413750d86f81191caf43d0b347b842c
lucas <santanafx@hotmail.com>
2024-11-03 12:48:27 -0300

    feat: added delete note route

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,26 @@
 import { Router } from "express";
 import { body } from "express-validator";
-import { createNewNote, getNotes } from "./controllers/note.controller";
+import {
+  createNewNote,
+  deleteNotes,
+  getNotes,
+} from "./controllers/note.controller";
 import { handleInputsErrors } from "./modules/middleware";
 
 const router = Router();
 
+router.get("/getNotes", getNotes);
 router.post(
   "/createNewNote",
   body("text").isString(),
   handleInputsErrors,
   createNewNote
 );
-
-router.get("/getNotes", getNotes);
+router.delete(
+  "/deleteNotes",
+  body("ids").isArray(),
+  handleInputsErrors,
+  deleteNotes
+);
 
 export default router;

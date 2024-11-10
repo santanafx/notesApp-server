79987832a89ef6f0b73dcc7eb1f39ae9fc002913
lucas <santanafx@hotmail.com>
2024-11-10 02:15:58 -0300

    refactor: added new routes and userId

--- src/router.ts
+++ src/router.ts
@@ -5,29 +5,44 @@ import {
   deleteNotes,
   getNotes,
   updateNote,
+  updateNotes,
 } from "./controllers/note.controller";
 import { handleInputsErrors } from "./modules/middleware";
 
 const router = Router();
 
-router.get("/getNotes", getNotes);
+router.get(
+  "/getNotes",
+  body("userId").isString(),
+  handleInputsErrors,
+  getNotes
+);
 router.post(
   "/createNewNote",
   body("text").isString(),
+  body("userId").isString(),
   handleInputsErrors,
   createNewNote
 );
 router.delete(
   "/deleteNotes",
   body("ids").isArray(),
+  body("userId").isString(),
   handleInputsErrors,
   deleteNotes
 );
 router.put(
   "/updateNote",
   body("text").isString(),
+  body("userId").isString(),
   handleInputsErrors,
   updateNote
+);
+router.put(
+  "/updateNotes",
+  body("userId").isString(),
+  handleInputsErrors,
+  updateNotes
 );
 
 export default router;
